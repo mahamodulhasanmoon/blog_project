781f4d42f2ba03cff2c3aedacf28dded4497778c
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-11-14 02:34:09 +0600

    home dashboard templating done

--- admin/template.php
+++ admin/template.php
@@ -10,7 +10,27 @@
             <div id="layoutSidenav_content">
                 <main>
 <div class="container-fluid">
-  <h1>hello world</h1>
+  <?php 
+  
+  if(isset($view)){
+    if ($view=='dashboard') {
+        include('view/dash_view.php');
+    }
+    if ($view=='add_post') {
+        include('view/addpost_view.php');
+    }
+    if ($view=='manage_post') {
+        include('view/managepost_view.php');
+    }
+    if ($view=='add_categories') {
+        include('view/addCat_view.php');
+    }
+    if ($view=='manage_categories') {
+        include('view/manageCat_view.php');
+    }
+  }
+
+  ?>
 </div>
                 </main>
                 <?php include_once('includes/footer.php') ?>
